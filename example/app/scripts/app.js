9319a3c30ac236f64431a2f81b358086369f1562
subbaram <subbaram.kummithi@acknotech.com>
2016-07-08 13:09:01 +0530

    Added Example file

---
+++ example/app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name exampleApp
+ * @description
+ * # exampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('exampleApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+	'ui.bootstrap',
+	'ui.map'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
